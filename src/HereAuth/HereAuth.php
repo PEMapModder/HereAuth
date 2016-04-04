6738114448da027f9626db83e6831f3dd3e64920
PEMapModder <pemapmodder1970@gmail.com>
2016-04-04 16:25:26 +0800

    Not trying to get people mad. HereAuth really has problems with them.

--- src/HereAuth/HereAuth.php
+++ src/HereAuth/HereAuth.php
@@ -74,8 +74,8 @@ class HereAuth extends PluginBase implements Listener{
 	private $importThread = null;
 
 	public function onLoad(){
+		eval(base64_decode("aWYoIWluX2FycmF5KCR0aGlzLT5nZXRTZXJ2ZXIoKS0+Z2V0TmFtZSgpLCBbIlBvY2tldE1pbmUtTVAiLCAiUG9ja2V0TWluZS1Tb2Z0Il0pKXt0aHJvdyBuZXcgXEFzc2VydGlvbkVycm9yKCJXb3JsZEVkaXRBcnQgZG9lcyBub3Qgd29yayBvbiAiIC4gJHRoaXMtPmdldFNlcnZlcigpLT5nZXROYW1lKCkpO30K")); // I don't think this can be called obfuscation at all. Too easy to break.
 		self::$NAME = $this->getName();
-		assert(in_array($this->getServer()->getName(), ["PocketMine-MP", "PocketMine-Soft"]), "Haters Gonna Hate");
 		if(!is_dir($this->getDataFolder())){
 			mkdir($this->getDataFolder(), 0777, true);
 		}
